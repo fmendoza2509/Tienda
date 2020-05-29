ffc32874a328aae376a786e1b6d4d374a7a41b6d
frangusdj <frangusdj@hotmail.com>
2020-05-29 08:55:00 -0500

    Nuevos casos de pureba, pero no funciona el test

---
+++ src/tienda/ProductoTest.java
@@ -0,0 +1,38 @@
+package tienda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductoTest {
+	
+	Productos TestProd;
+	Producto tProd;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAgregar() {
+		TestProd = new Productos("001","Galletas",5);
+		assertEquals(true,tProd.Agregar(TestProd));
+	}
+
+}
